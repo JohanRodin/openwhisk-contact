57eb06d008739ea6611dcc548a4b41b99630928c
JohanRodin <rodinjohan@gmail.com>
2017-05-02 15:29:37 +0200

    cut down on domain

--- app/controllers/requestController.js
+++ app/controllers/requestController.js
@@ -17,7 +17,7 @@ angular.module('ngWhisk')
             var config = {
                 headers : {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;',
-                    'Access-Control-Allow-Origin': 'https://6e28847c-de9b-4446-8207-c2fadad1d143-gws.api-gw.mybluemix.net',
+                    'Access-Control-Allow-Origin': '*.mybluemix.net',
                     'Access-Control-Allow-Credentials': 'True',
                     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
                 }
