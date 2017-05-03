abf80ed9e063ceef58a78df6e43e659c2f659b25
JohanRodin <rodinjohan@gmail.com>
2017-05-03 17:18:18 +0200

    back

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 var connect = require('connect');
 
-var app = connect()
-
-    .use(connect.logger('dev'))
-
-    .use(connect.static('home'))
-
-    .use(function(req, res){
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end('hello world\n');
-
- });
-
 var serveStatic = require('serve-static');
+
 var port = (process.env.VCAP_APP_PORT || 3000);
+
 var host = (process.env.VCAP_APP_HOST || 'localhost');
-app().use(serveStatic(__dirname)).listen(port,host);
+
+connect().use(serveStatic(__dirname)).listen(port,host);
