5a4de0648f040e16174e6fd95b107d1d5ff0e25d
JohanRodin <rodinjohan@gmail.com>
2017-05-04 09:30:33 +0200

    removed header stuff

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 
-var app = connect().use(function(req, res){
-    res.setHeader("Access-Control-Allow-Origin", "*");
+connect().use(serveStatic(__dirname)).listen(port,host);
+
+//var app = connect().use(function(req, res){
+//    res.setHeader("Access-Control-Allow-Origin", "*");
     //res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
- });
-app().use(serveStatic(__dirname)).listen(port,host);
+// });
+//app().use(serveStatic(__dirname)).listen(port,host);
