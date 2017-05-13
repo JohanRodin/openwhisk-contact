1e58ebe302a5825d96764f54044675148146b47d
JohanRodin <rodinjohan@gmail.com>
2017-05-13 22:05:51 +0200

    delete comment

--- server.js
+++ server.js
@@ -7,9 +7,3 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 
 connect().use(serveStatic(__dirname)).listen(port,host);
-
-//var app = connect().use(function(req, res){
-//    res.setHeader("Access-Control-Allow-Origin", "*");
-    //res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-// });
-//app().use(serveStatic(__dirname)).listen(port,host);
